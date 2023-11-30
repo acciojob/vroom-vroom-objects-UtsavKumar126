2cd2681e34abcf512c05cca21b4728bf3d8e6dd0
UtsavKumar126 <117590564+UtsavKumar126@users.noreply.github.com>
2023-11-30 20:53:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,16 +4,20 @@ function Car(make, model) {
 	this.model=model	
 }
 Car.prototype.getMakeModel = function() {
-	return `${this.make} ${this.model}`;
+	return `${this.make}+' '+${this.model}`;
 }
 
 function SportsCar(make, model, topSpeed) {
 	this.topSpeed=topSpeed;
 	Car.call(this,make,model);
 }
-SportsCar.prototype.getTopSpeed=function(){
-	return this.topSpeed;
-}
+SportsCar.prototype = Object.create(Car.prototype);
+SportsCar.prototype.constructor = SportsCar;
+
+// Method added to SportsCar prototype
+SportsCar.prototype.getTopSpeed = function () {
+  return this.topSpeed;
+};
 
 // Do not change the code below
 window.Car = Car;
